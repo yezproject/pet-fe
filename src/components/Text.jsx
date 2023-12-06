49c87bc12c31745f348e1d4a892c0e3b63b83546
Chó <diennm.hn@gmail.com>
2023-12-06 19:44:45 +0700

    install vitest

---
+++ src/components/Text.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Text() {
+  return (
+    <>
+      <div>
+        <h1>Vite</h1>
+      </div>
+    </>
+  );
+}
+
+export default Text;
