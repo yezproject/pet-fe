805d02f12648a8794f220f5d19fe374014f0a375
Charlie Nguyen <vawntanng@gmail.com>
2023-12-13 15:53:20 +0700

    modify config tailwind and add react-hook-form, prop-types

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './components/**/*.{html,js,jsx}',
-    './pages/**/*.{html,js,jsx}',
-    './index.html',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'auth-background': 'url("./src/images/bg-auth.png")',
+        'ic-logo': 'url("./src/images/icons/ic_logo.svg")',
+      },
+    },
+    colors: {
+      darkColor: '#000000',
+      whiteColor: '#FFFFFF',
+      grayColor: '#747A80',
+      primaryColor: '#0052FF',
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
   },
   plugins: [],
 };
