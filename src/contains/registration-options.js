d67cbf76b02832032a157418dfa81a079767159e
Charlie Nguyen <vawntanng@gmail.com>
2023-12-17 17:52:54 +0700

    [ADD] add join page

---
+++ src/contains/registration-options.js
@@ -0,0 +1,10 @@
+export const options = [
+  { label: 'Select your region', value: '' },
+  { label: 'Viet Nam', value: 'viet_nam' },
+  { label: 'North America', value: 'north_america' },
+  { label: 'Europe', value: 'europe' },
+  { label: 'Asia', value: 'asia' },
+  { label: 'South America', value: 'south_america' },
+  { label: 'Africa', value: 'africa' },
+  { label: 'Oceania', value: 'oceania' },
+];
