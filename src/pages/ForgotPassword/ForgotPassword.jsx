98bb2b9a0d5cc5912c1ab38e0b16fa507b28a581
Charlie Nguyen <vawntanng@gmail.com>
2023-12-19 21:01:58 +0700

    (fix-up)

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -23,14 +23,6 @@ const ForgotPassword = () => {
     console.log(data);
   };
 
-  // const Icon = ({ src, onClick }) => {
-  //   const handleClick = () => {
-  //     onClick();
-  //   };
-  //   return (
-  //     <img className='h-[18px] w-[18px] object-contain cursor-pointer' src={src} alt='icon' onClick={handleClick} />
-  //   );
-  // };
   return (
     <Auth>
       <div className='mb-12 mt-[104px]'>
