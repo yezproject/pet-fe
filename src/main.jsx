b5fbc599044665cdc491443d46ec0d722b38bf3c
Darius <anh0conanvip@gmail.com>
2024-05-18 16:40:39 +0700

    [ADD] Add pages, login route

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/App.jsx';
 
-import 'antd/dist/reset.css';
 import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
