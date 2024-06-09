1d332dd729c0913ae5719667865062c4278f956c
nhokli1102 <84114858+nhokli1102@users.noreply.github.com>
2024-06-09 21:52:15 +0700

    [ADD] Signup phén phén (#26)

--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -1,91 +1,100 @@
 import * as React from 'react';
-import Box from '@mui/joy/Box';
+import {useState} from 'react';
 import Button from '@mui/joy/Button';
-import Divider from '@mui/joy/Divider';
 import FormControl from '@mui/joy/FormControl';
 import FormLabel from '@mui/joy/FormLabel';
-import Link from '@mui/joy/Link';
 import Input from '@mui/joy/Input';
-import Typography from '@mui/joy/Typography';
 import Stack from '@mui/joy/Stack';
-import GoogleIcon from '../../common/icon/GoogleIcon';
+import Typography from "@mui/joy/Typography";
+import Link from "@mui/joy/Link";
+import GoogleIcon from "@/common/icon/GoogleIcon.jsx";
+import Divider from "@mui/joy/Divider";
+import {useNavigate} from "react-router-dom";
+import {signUp} from "@/services/join-service.js";
 
-export default function SignUp() {
+export default function SignUp(props) {
+    const navigate = useNavigate();
+    const [acceptPassword, setAcceptPassword] = useState(true)
+    const [getErrorUser, setErrorUser] = useState(false);
+    const toSignIn = () => {
+        navigate("/");
+    }
+
+    const onRestrict = async (event) => {
+        event.preventDefault();
+        const formElements = event.currentTarget.elements;
+        const isAcceptPassword = formElements.enter.value === formElements.password.value;
+        setAcceptPassword(isAcceptPassword);
+        if (isAcceptPassword) {
+            const formData = {
+                fullName: formElements.fullName.value,
+                email: formElements.email.value,
+                password: formElements.password.value,
+            };
+            const {status} = await signUp(formData);
+            setErrorUser(status !== 201)
+            if (status === 201) {
+                toSignIn();
+                props.showToast('Bạn đã đăng ký thành công, vui lòng đăng nhập');
+            }
+        }
+    }
+
     return (
-        <Box component="signup"
-             sx={{
-                 my: 'auto',
-                 py: 2,
-                 pb: 5,
-                 display: 'flex',
-                 flexDirection: 'column',
-                 gap: 2,
-                 width: 400,
-                 maxWidth: '100%',
-                 mx: 'auto',
-                 borderRadius: 'sm',
-                 '& form': {
-                     display: 'flex',
-                     flexDirection: 'column',
-                     gap: 2,
-                 },
-                 [`& .MuiFormLabel-asterisk`]: {
-                     visibility: 'hidden',
-                 },
-             }}>
-            <Stack gap={4} sx={{ mb: 2 }}>
+        <>
+            <Stack gap={4} sx={{mb: 2}}>
                 <Stack gap={1}>
                     <Typography component="h1" level="h3">
-                        Sign in
+                        Sign up
                     </Typography>
                     <Typography level="body-sm">
-                        New to company?{' '}
-                        <Link href="#replace-with-a-link" level="title-sm">
-                            Sign up!
+                        Do you already have an account?{' '}
+                        <Link level="title-sm" onClick={() => toSignIn()}>
+                            Sign in!
                         </Link>
                     </Typography>
                 </Stack>
                 <Button variant="soft"
                         color="neutral"
                         fullWidth
-                        startDecorator={<GoogleIcon />}>
+                        startDecorator={<GoogleIcon/>}>
                     Continue with Google
                 </Button>
             </Stack>
             <Divider
                 sx={theme => ({
                     [theme.getColorSchemeSelector('light')]: {
-                        color: { xs: '#FFF', md: 'text.tertiary' },
+                        color: {xs: '#FFF', md: 'text.tertiary'},
                     },
                 })}
             >
                 or
             </Divider>
-            <Stack gap={4} sx={{ mt: 2 }}>
-                <form onSubmit={event => onSignUp(event)}>
+            <Stack gap={4} sx={{mt: 2}}>
+                <form onSubmit={event => onRestrict(event)}>
                     <FormControl required>
                         <FormLabel>Full name</FormLabel>
-                        <Input type="text" name="fullname"/>
+                        <Input type="text" name="fullName"/>
                     </FormControl>
-                    <FormControl required>
+                    <FormControl required error={getErrorUser}>
                         <FormLabel>Email</FormLabel>
                         <Input type="email" name="email"/>
                     </FormControl>
-                    <FormControl required>
+                    <FormControl required error={!acceptPassword}>
                         <FormLabel>Password</FormLabel>
                         <Input type="password" name="password"/>
                     </FormControl>
-                    <FormControl required>
+                    <FormControl required error={!acceptPassword}>
                         <FormLabel>Enter the password</FormLabel>
-                        <Input type="password" name="password"/>
+                        <Input type="password" name="enter"/>
                     </FormControl>
-                    <Stack gap={4} sx={{ mt: 2 }}>
+                    <Stack gap={4} sx={{mt: 2}}>
                         <Button type="submit">
                             Sign up
                         </Button>
                     </Stack>
                 </form>
             </Stack>
-        </Box>
+        </>
     );
 }
